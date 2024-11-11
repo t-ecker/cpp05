180ceca78aaf28a109ba61b1162f50962a9f08a0
t-ecker <152566019+t-ecker@users.noreply.github.com>
2024-11-12 00:34:19 +0100

    added ex00

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,43 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string.h>
+#include <iostream>
+
+#define MIN_GRADE	150
+#define MAX_GRADE	0
+
+class Bureaucrat
+{
+	private:
+		const std::string _name;
+		int _grade;
+	
+	public:
+		Bureaucrat();
+		Bureaucrat(int grade, std::string name);
+		Bureaucrat(const Bureaucrat &src);
+        Bureaucrat &operator=(const Bureaucrat &src);
+		~Bureaucrat();
+
+		std::string getName(void) const;
+		int getGrade(void) const;
+		void increment(void);
+		void decrement(void);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const noexcept;
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const noexcept;
+		};
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &Bureaucrat);
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,62 @@
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+    try
+    {
+		std::cout << "creating Ben" << std::endl;
+        Bureaucrat a(0, "Ben");
+        std::cout << a << std::endl;
+        try
+        {
+            std::cout << "Incrementing " << a.getName() << "'s grade..." << std::endl;
+            a.increment();
+        }
+        catch (std::exception& e)
+        {
+            std::cout << "Error: " << e.what() << std::endl;
+        }
+        std::cout << a << std::endl;
+
+        std::cout << std::endl;
+
+		std::cout << "creating Tim" << std::endl;
+        Bureaucrat c(150, "Tim");
+        std::cout << c << std::endl;
+        try
+        {
+            std::cout << "Decrementing " << c.getName() << "'s grade..." << std::endl;
+            c.decrement();
+        }
+        catch (std::exception& e)
+        {
+            std::cout << "Error: " << e.what() << std::endl;
+        }
+        std::cout << c << std::endl;
+
+        std::cout << std::endl;
+
+        std::cout << a << std::endl;
+        try
+        {
+            std::cout << "Decrementing " << a.getName() << "'s grade ..." << std::endl;
+            a.decrement();
+        }
+        catch (std::exception& e)
+        {
+  		      std::cout << "Error: " << e.what() << std::endl;
+        }
+        std::cout << a << std::endl;
+
+        std::cout << std::endl;
+
+		std::cout << "creating Sam" << std::endl;
+        Bureaucrat b(152, "Sam");
+    }
+    catch(std::exception& e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
